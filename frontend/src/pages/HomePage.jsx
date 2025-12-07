78c17b792be201ce456210917c0bb368129f7cbe
Srihas Inaganti <srihasinaganti@gmail.com>
2025-12-07 17:55:33 -0500

    added snow effects to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,38 +1,43 @@
+import {SnowForeground, SnowBackground} from "../components/Snow.jsx";
+
 export default function HomePage() {
-    return (
-      <div
-        style={{
-          width: "100vw",
-          height: "100vh",     // full height, no scrolling
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-  
-        <h1 style={{ fontSize: "42px", fontWeight: "700", textDecoration: "underline" }}>
-          Good Deed Secret Santa
-        </h1>
-  
-        <div style={{
-          display: "flex",
-          gap: "60px",
-          marginTop: "60px",
-          alignItems: "center"
-        }}>
-          <img src="/tree.png" alt="tree" style={{ height: "200px" }} />
-  
-          <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-            <button style={btn}>Create a new Group</button>
-            <button style={btn}>Log into your Group</button>
-          </div>
-  
-          <img src="/tree.png" alt="tree" style={{ height: "200px" }} />
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1 style={{ fontSize: "42px", fontWeight: "700", textDecoration: "underline" }}>
+        Good Deed Secret Santa
+      </h1>
+
+      <div style={{
+        display: "flex",
+        gap: "60px",
+        marginTop: "60px",
+        alignItems: "center"
+      }}>
+        <img src="/tree.png" alt="tree" style={{ height: "200px" }} />
+
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <button style={btn}>Create a new Group</button>
+          <button style={btn}>Log into your Group</button>
         </div>
+
+        <img src="/tree.png" alt="tree" style={{ height: "200px" }} />
       </div>
-    );
-  }
+
+      {/*Snow Effects*/}
+      <SnowForeground />
+      <SnowBackground />
+    </div>
+  );
+}
   
   const btn = {
     background: "black",
