489e86f17be42d689fd1ee00de7fbddb0ecd7e30
gabbycheetham <cheetham@terpmail.umd.edu>
2025-12-07 18:13:10 -0500

    debug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StrictMode } from 'react' 
+import { createRoot } from 'react-dom/client' 
+import './index.css' 
+import HomePage from './pages/HomePage.jsx'
 
-import "./index.css";
-import HomePage from "./pages/HomePage.jsx";
-import YourGoodDeed from "./pages/YourGoodDeed.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/gooddeed" element={<YourGoodDeed />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+createRoot(document.getElementById('root')).render (
+   <StrictMode> 
+    <HomePage /> 
+    </StrictMode> 
+  )
